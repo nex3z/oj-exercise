423f9bf12692a5d61ac60a800ffc0d02ced594b6
nex3z <litianxing9@gmail.com>
2015-08-30 13:40:14 +0800

    Update.

--- HUST/Virtual Judge E3/D - Jobbery/Jobbery.cpp
+++ HUST/Virtual Judge E3/D - Jobbery/Jobbery.cpp
@@ -7,98 +7,109 @@ typedef short Connection[MAX_N][MAX_N];
 
 void solveE3d_Jobbery();
 short inputConnection(Connection connection);
-void tarjan(int u);
+void tarjan(int n);
+void strongConnect(int v);
 
 int main() {
-    solveE3d_Jobbery();
-    return 0;
+	solveE3d_Jobbery();
+	return 0;
 }
 
 Connection connection;
-short sccCnt = 0;
-short dfn[MAX_N], sccLUT[MAX_N], in[MAX_N];
+int sccIdLut[MAX_N], sccInDegree[MAX_N], sccIdCnt = 0;
 void solveE3d_Jobbery() {
-    short n = inputConnection(connection);
+	short n = inputConnection(connection);
 
-    for (int i = 1; i <= n; ++i) {
-        if (dfn[i] == 0) {
-            tarjan(i);
-        }
-    }
+	tarjan(n);
 
-    for (int i = 1; i <= n; ++i) {
-        int sccId = sccLUT[i];
-        for (int j = 0; connection[i][j] != 0; ++j) {
-            if (sccLUT[i] != sccLUT[connection[i][j]])
-                ++in[sccLUT[connection[i][j]]];
-        }
-    }
+	for (int i = 1; i <= n; ++i) {
+		int sccId = sccIdLut[i];
+		for (int j = 0; connection[i][j] != 0; ++j) {
+			if (sccIdLut[i] != sccIdLut[connection[i][j]])
+				++sccInDegree[sccIdLut[connection[i][j]]];
+		}
+	}
 
-    short zeroInDegId, zeroInDegCnt = 0;
-    for (int i = 1; i <= sccCnt; ++i) {
-        if (in[i] == 0) {
-            zeroInDegId = i;
-            ++zeroInDegCnt;
-        }
-    }
+	short zeroInDegId, zeroInDegCnt = 0;
+	for (int i = 1; i <= sccIdCnt; ++i) {
+		if (sccInDegree[i] == 0) {
+			zeroInDegId = i;
+			++zeroInDegCnt;
+		}
+	}
 
-    if (zeroInDegCnt != 1) {
-        printf("0\n");
-        return;
-    }
+	if (zeroInDegCnt != 1) {
+		printf("0\n");
+		return;
+	}
 
-    for (int i = 1; i <= n; ++i) {
-        if (sccLUT[i] == zeroInDegId) {
-            printf("%d ", i);
-        }
-    }
-    printf("0\n");
+	for (int i = 1; i <= n; ++i) {
+		if (sccIdLut[i] == zeroInDegId) {
+			printf("%d ", i);
+		}
+	}
+	printf("0\n");
+
 }
 
 short inputConnection(Connection connection) {
-    int n;
-    scanf("%d", &n);
+	int n;
+	scanf("%d", &n);
 
-    int senateId, cnt = 0;
-    for (int i = 1; i <= n;) {
-        scanf("%d", &senateId);
-        connection[i][cnt++] = (short)senateId;
-        if (senateId == 0) {
-            ++i;
-            cnt = 0;
-        }
-    }
+	int senateId, cnt = 0;
+	for (int i = 1; i <= n;) {
+		scanf("%d", &senateId);
+		connection[i][cnt++] = (short)senateId;
+		if (senateId == 0) {
+			++i;
+			cnt = 0;
+		}
+	}
 
-    return n;
+	return n;
 }
 
-short low[MAX_N], stack[MAX_N], index = 1, top = 0;
-bool inStack[MAX_N];
-void tarjan(int u) {
-    dfn[u] = low[u] = ++index;
-    stack[++top] = u;
-    inStack[u] = true;
+int lowLink[MAX_N], stack[MAX_N], index[MAX_N], indexCnt = 0, top = 0;
+bool onStack[MAX_N];
+void tarjan(int n) {
+	for (int i = 1; i <= n; ++i) {
+		onStack[i] = false;
+		index[i] = 0;
+		sccIdLut[i] = 0;
+	}
 
-    for (int i = 0; connection[u][i] != 0; ++i) {
-        short currentId = connection[u][i];
-        if (dfn[currentId] == 0) {
-            tarjan(currentId);
-            low[u] = low[u] < low[currentId] ? low[u] : low[currentId];
-        }
-        else {
-            if (inStack[currentId]) {
-                low[u] = low[u] < dfn[currentId] ? low[u] : dfn[currentId];
-            }
-        }
-    }
+	indexCnt = 0;
+	top = 0;
 
-    short id;
-    if (dfn[u] == low[u]) {
-        ++sccCnt;
-        do {
-            id = stack[top--];
-            sccLUT[id] = sccCnt;
-            inStack[id] = false;
-        } while (u != id);
-    }
+	for (int v = 1; v <= n; ++v) {
+		if (index[v] == 0) {
+			strongConnect(v);
+		}
+	}
+}
+
+void strongConnect(int v) {
+	index[v] = lowLink[v] = ++indexCnt;
+	stack[++top] = v;
+	onStack[v] = true;
+
+	for (int i = 0; connection[v][i] != 0; ++i) {
+		int w = connection[v][i];
+		if (index[w] == 0) {
+			strongConnect(w);
+			lowLink[v] = lowLink[v] < lowLink[w] ? lowLink[v] : lowLink[w];
+		} else if (onStack[w]) {
+			lowLink[v] = lowLink[v] < index[w] ? lowLink[v] : index[w];
+		}
+	}
+
+	if (lowLink[v] == index[v]) {
+		++sccIdCnt;
+		int w;
+		do {
+			w = stack[top--];
+			onStack[w] = false;
+			sccIdLut[w] = sccIdCnt;
+		} while (v != w);
+	}
 }
